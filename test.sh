24f2095bfc41a38ea3069e1d54c89e2f73fa30f3
TonyFenng <simazhuo@qq.com>
2021-01-12 17:47:02 +0800

    Delete test.sh

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-D1=`date -d '1 days ago' +%Y-%m-%d`
-D1S=`date -d '1 days ago' +%m%d`
-D2=`date +%Y%m%d`
-mysqldump -utony -ptony tony seed --where="created_at between '${D1}' and '${D2}'" > ${D1S}.sql
-zip ${D1S}.zip ${D1S}.sql
